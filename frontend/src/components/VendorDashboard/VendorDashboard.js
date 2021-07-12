9015dcf9c89e4c671f840cee8758450a4056e6de
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-12 17:33:33 -0500

    main dashboard component

---
+++ frontend/src/components/VendorDashboard/VendorDashboard.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import PeopleIcon from '@material-ui/icons/People';
+import MessageIcon from '@material-ui/icons/Message';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+import WarningIcon from '@material-ui/icons/Warning';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import { Link } from 'react-router-dom';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  // necessary for content to be below app bar
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3)
+  },
+  
+}));
+
+export default function VendorDashboard() {
+  const classes = useStyles();
+ 
+//   handleSelect(ranges){ console.log(ranges) }
+
+  return (
+    <div className={classes.root}>
+     
+      <AppBar  className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" noWrap>
+            Vendor Order Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        className={classes.drawer}
+        variant="permanent"
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+        anchor="left"
+      >
+        <div className={classes.toolbar} />
+        <Divider />
+        <List>
+        <ListItem component={ Link } to="/vendorDashboard" button key={'Home'}>
+              <ListItemIcon><DashboardIcon /></ListItemIcon>
+              <ListItemText primary={'Home'} />
+          </ListItem>
+          <ListItem component={ Link } to="/vendor/currentOrders" button key={'Current Orders'}>
+              <ListItemIcon><WarningIcon /></ListItemIcon>
+              <ListItemText primary={'Current Orders'} />
+          </ListItem>
+          <ListItem component={ Link } to="/vendor/pastOrders" button key={'Past Orders'}>
+              <ListItemIcon><PeopleIcon /></ListItemIcon>
+              <ListItemText primary={'Past Orders'} />
+          </ListItem>
+          <ListItem button key={'Messages'}>
+              <ListItemIcon><MessageIcon /></ListItemIcon>
+              <ListItemText primary={'Messages'} />
+          </ListItem>
+          <ListItem button key={'My Tasks'}>
+              <ListItemIcon><AssignmentIcon /></ListItemIcon>
+              <ListItemText primary={'My Tasks'} />
+          </ListItem>
+        </List>
+        <Divider />
+
+      </Drawer>
+    </div>
+  );
+}
