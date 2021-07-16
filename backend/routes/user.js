c5881cd2e0654e5c5d8965d347ae3c4e2d3d8451
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-16 12:36:09 -0500

    added more user routes that use points, creating an order. working on order detail

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -13,5 +13,45 @@ router.put("/updateAddress", security.requireAuthenticatedUser, async (req, res,
     }
   })
 
+  router.get("/getRewards", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const rewards = await User.getRewardPoints({user})
+      return res.status(200).json({ rewards })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.get("/getOrders", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const rewards = await User.listOrdersForUser({user})
+      return res.status(200).json({ rewards })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.put("/addRewards", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const rewards = await User.addRewardPoints({user, pizzasPurchased: req.body})
+      return res.status(200).json({ rewards })
+    } catch (err) {
+      next(err)
+    }
+  })
+
+  router.put("/redeemRewards", security.requireAuthenticatedUser, async (req, res, next) => {
+    try {
+      const user = res.locals.user
+      const rewards = await User.removeRewardPoints({user, pointsRedeemed: req.body})
+      return res.status(200).json({ rewards })
+    } catch (err) {
+      next(err)
+    }
+  })
+
 
   module.exports = router
