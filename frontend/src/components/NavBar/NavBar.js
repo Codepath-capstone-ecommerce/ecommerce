626080893a29f4d12a84bd94a210d8c29e65f0bf
Diego Saldonid <dsaldoni@ucsd.edu>
2021-07-19 08:34:17 -0700

    navbar can now navigate pages

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -3,25 +3,28 @@ import { useNavigate, Link } from "react-router-dom"
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-      '& > *': {
-        margin: theme.spacing(1),
-      },
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
     },
-  }));
+  },
+}));
 
-export default function NavBar(){
-    const classes = useStyles()
-    return(
-        <Box display="flex" justifyContent="space-between" m={3} px={15}>
-            <img src = "http://codepath-lifetracker.surge.sh/static/media/codepath.70a9a31f.svg" alt = "logo" width="40px" height="40px"></img>
-            <Box className={classes.root}>
-                <Button variant="outlined">Menus</Button>
-                <Button variant="outlined">Our Story</Button>
-                <Button variant="outlined" >Login</Button>
-                <Button variant="outlined">Order Now</Button>
-            </Box>
-        </Box>
 
-    )
+export default function NavBar() {
+  const navigate = useNavigate()
+  const classes = useStyles()
+  return (
+    <Box display="flex" justifyContent="space-between" m={3} px={15}>
+      <img src="http://codepath-lifetracker.surge.sh/static/media/codepath.70a9a31f.svg" alt="logo" width="40px" height="40px"></img>
+      <Box className={classes.root}>
+        <Button onClick={() => { navigate("/menu") }} variant="outlined">Menus</Button>
+        <Button onClick={() => { navigate("/aboutUs") }} variant="outlined">Our Story</Button>
+        <Button onClick={() => { navigate("/order") }} variant="outlined">Order Now</Button>
+        <Button onClick={() => { navigate("/login") }} variant="outlined" >Login</Button>
+        <Button onClick={() => { navigate("/signup") }} variant="outlined" >Sign Up</Button>
+      </Box>
+    </Box>
+
+  )
 }
