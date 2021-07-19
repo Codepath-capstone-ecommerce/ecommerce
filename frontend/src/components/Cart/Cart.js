d5414c6cc989b8102c6a6ebf16fd582337ed35ba
Diego Saldonid <dsaldoni@ucsd.edu>
2021-07-19 15:37:38 -0700

    added cart functionality

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,5 +1,6 @@
 import { useAppStateContext } from '../../contexts/appStateContext';
 import { Button } from '@material-ui/core';
+import apiClient from '../../services/apiClient';
 
 export default function Cart() {
     const { appState } = useAppStateContext()
@@ -22,8 +23,26 @@ export default function Cart() {
         items.push({ name: key, quantity: value });
     }
 
-    const printOb = () => {
-        console.log(obj)
+    const cart = []
+    for (const [key, value] of Object.entries(obj)) {
+        let obj = {}
+        obj[key] = value
+        cart.push(obj);
+    }
+
+    const createOrder = async () => {
+        console.log(cart)
+        const { data, error } = await apiClient.createOrder(
+            {
+                "cart": {
+                    address: "123 Street",
+                    products: cart
+                }
+            }
+        )
+        // if (error){
+        //   setErrors((e) => ({ ...e, form:error}))
+        // }
     }
 
     return (
@@ -31,7 +50,7 @@ export default function Cart() {
             {items.length === 0 ? "Cart is empty" : items.map((item, idx) => (
                 <div key={idx}>Item:{item.name} Quantity:{item.quantity}</div>
             ))}
-            <Button onClick={printOb}>Place Order</Button>
+            <Button onClick={createOrder}>Place Order</Button>
         </div>
 
 
