f10d317afccad8d11906e162d586728ffd93c6db
Diego Saldonid <dsaldoni@ucsd.edu>
2021-07-21 15:32:28 -0700

    it's alive!!

--- frontend/src/components/Cart/Cart.js
+++ frontend/src/components/Cart/Cart.js
@@ -1,9 +1,22 @@
 import { useAppStateContext } from '../../contexts/appStateContext';
-import { Button } from '@material-ui/core';
+import { Button, Typography, Card,CardContent,CardMedia } from '@material-ui/core';
 import apiClient from '../../services/apiClient';
+import { useState, useEffect } from "react"
+import axios from "axios"
+import CartCard from '../CartCard/CartCard';
 
 export default function Cart() {
     const { appState } = useAppStateContext()
+    const [isCartFilled, setCart] = useState(false)
+    
+    // if (appState.cart.length !==0){
+    //     setCart(true)
+    // }
+
+    // const emptyCart = () =>{
+    //     setCart(false)
+    // }
+
     const formatCart = () => {
         const obj = {}
         for (let i = 0; i < appState.cart.length; i++) {
@@ -44,13 +57,22 @@ export default function Cart() {
         //   setErrors((e) => ({ ...e, form:error}))
         // }
     }
+    // <Card key={idx}>
+//     <CardContent>
+//     <Typography>Item: {item.name}</Typography>
+//     <Typography>Quantity: {item.quantity}</Typography>
+// </CardContent>
+
+// </Card>
 
     return (
         <div>
+            <Typography>Your Cart</Typography>
             {items.length === 0 ? "Cart is empty" : items.map((item, idx) => (
-                <div key={idx}>Item:{item.name} Quantity:{item.quantity}</div>
+                <CartCard product={item}></CartCard>
             ))}
             <Button onClick={createOrder}>Place Order</Button>
+            <Button>Clear Cart</Button>
         </div>
 
 
