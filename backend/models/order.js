f370b38b82e5873b058df754918072a200f0fc32
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-22 15:46:13 -0500

    updated function name change

--- backend/models/order.js
+++ backend/models/order.js
@@ -22,6 +22,7 @@ class Order {
   }
 
   static async fetchOrderDetailById(orderId) {
+    
     const result = await db.query(
       `
           SELECT order_detail.order_id AS "order_detail_id",
@@ -35,6 +36,17 @@ class Order {
     );
 
     return result.rows;
+  }
+
+  static async completeOrderDetail(orderId) {
+    
+    const query = `
+    UPDATE order_detail
+    SET completed = TRUE
+    WHERE order_detail.order_id  = $1`
+    const result = await db.query(query, [orderId.orderId])
+
+    return orderId.orderId + ": order has been updated!"
   }
 
   static async fetchAllOrderDetail() {
