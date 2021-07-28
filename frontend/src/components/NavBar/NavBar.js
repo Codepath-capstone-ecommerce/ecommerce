4a872bdcd74b5455688bd8cae8678f92ae7979e1
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-07-28 17:24:05 -0500

    updated nav bar with style, removed some padding and added colors

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,13 +4,27 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useAppStateContext } from '../../contexts/appStateContext';
 import UserDrawer from "../UserDrawer/UserDrawer";
 import apiClient from '../../services/apiClient';
-
+import classnames from 'classnames';
+import { white } from "color-name";
+import './NavBar.css'; 
+import pizzaImg from "../../assets/pizza.png"
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
       margin: theme.spacing(1),
+      backgroundColor: "#fffff"
     },
   },
+  customButton: {
+    border: "3px solid",
+    borderColor: "#2EDBFD !important",
+    backgroundColor: "#000000",
+    color: "white !important",
+    "&:hover": {
+      color: "black !important",
+      backgroundColor: "#2EDBFD",
+    }
+  }
 }));
 
 
@@ -28,17 +42,19 @@ export default function NavBar() {
 
   }
   return (
-    <Box display="flex" justifyContent="space-between" m={3} px={15}>
-      <img src="http://codepath-lifetracker.surge.sh/static/media/codepath.70a9a31f.svg" alt="logo" width="40px" height="40px"></img>
+    <Box display="flex" justifyContent="space-between" mt={1} px={5}>
+      <Link to={"/"}>
+      <img src={pizzaImg} id="pizza" alt="logo" ></img>
+      </Link>
       <Box className={classes.root}>
-        <Button onClick={() => { navigate("/menu") }} variant="outlined">Menus</Button>
-        <Button onClick={() => { navigate("/aboutUs") }} variant="outlined">Our Story</Button>
-        <Button onClick={() => { navigate("/order") }} variant="outlined">Order Now</Button>
+        <Button onClick={() => { navigate("/menu") }} variant="outlined" className={classnames(classes.customButton, "glow-button")}>Menus</Button>
+        <Button onClick={() => { navigate("/aboutUs") }} variant="outlined" className={classnames(classes.customButton, "glow-button")}>Our Story</Button>
+        <Button onClick={() => { navigate("/order") }} variant="outlined" className={classnames(classes.customButton, "glow-button")}>Order Now</Button>
         {appState.isAuthenticated ? <UserDrawer></UserDrawer> :
-          <Button onClick={() => { navigate("/login") }} variant="outlined" >Login</Button>
+          <Button onClick={() => { navigate("/login") }} variant="outlined" className={classnames(classes.customButton, "glow-button")} >Login</Button>
         }
-        {appState.isAuthenticated ? <Button onClick={emptyUser} variant="outlined">Log Out</Button> :
-          <Button onClick={() => { navigate("/signup") }} variant="outlined" >Sign Up</Button>
+        {appState.isAuthenticated ? <Button onClick={emptyUser} variant="outlined" className={classnames(classes.customButton, "glow-button")}>Log Out</Button> :
+          <Button onClick={() => { navigate("/signup") }} variant="outlined" className={classnames(classes.customButton, "glow-button")} >Sign Up</Button>
         }
       </Box>
     </Box>
