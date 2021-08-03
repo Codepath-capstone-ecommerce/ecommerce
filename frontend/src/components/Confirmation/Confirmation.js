fed22d6851695e54529fd70662ec716690cd76d6
Diego Saldonid <dsaldoni@ucsd.edu>
2021-08-03 08:28:22 -0700

    added confetti!

---
+++ frontend/src/components/Confirmation/Confirmation.js
@@ -0,0 +1,13 @@
+import useWindowSize from 'react-use-window-size'
+import Confetti from 'react-confetti'
+
+export default function Confirmation() {
+    // const { width,height} = useWindowSize()
+    // console.log(width,height)
+    return(
+        <Confetti
+        // width={width}
+        // height={height}
+        />
+    )
+}
