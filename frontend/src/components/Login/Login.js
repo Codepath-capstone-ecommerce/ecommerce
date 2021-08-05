721098ed7efecfbe25b19038feb073866f88594a
Kordell Schrock <44933349+kschrock@users.noreply.github.com>
2021-08-05 17:47:03 -0500

    updated UI

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,15 +1,67 @@
 import Button from '@material-ui/core/Button';
 import { useState } from "react"
-import { Grid, Paper, Avatar, TextField, Typography, Link,Box } from '@material-ui/core';
+import { Grid, Paper, Avatar, TextField, Typography,makeStyles,Box, InputAdornment, IconButton } from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { useNavigate } from "react-router-dom";
 import apiClient from '../../services/apiClient';
 import { useAppStateContext } from '../../contexts/appStateContext';
-
+import "./Login.css"
+import Divider from '@material-ui/core/Divider';
+import classNames from 'classnames'; //css and material ui style
+import { FaApple } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import { Link } from 'react-router-dom';
+import DividerWithText from '../DividerWithText/DividerWithText'
+const useStyles = makeStyles((theme) => ({
+    root: {
+        minWidth: 275,
+        marginBottom: 100,
+      },
+      title: {
+        fontSize: 30,
+        fontWeight: "900"
+      },
+      navbar : {
+        zIndex: "2 !important"
+      },
+      overlay: {
+        position: 'absolute',
+        color: 'white',
+        paddingTop: "15px",
+        paddingRight: "3px"
+     },
+     overlay2: {
+        position: 'absolute',
+        color: 'white',
+     },
+     mainBackground: {
+         backgroundColor: "#000",
+         width: '100vw',
+         height: '100vh',
+     },
+     blue: {
+        backgroundColor: "#2EDBFD",
+        paddingTop: 2,
+        paddingBottom: 2,
+    },
+    outlinePaper: { 
+        borderRadius: 20, borderColor: "#2EDBFD", padding: 10,
+        border: `4px solid`
+    },
+    notchedOutline: {
+        borderWidth: "2px",
+        borderColor: "black !important"
+      }
+}));
 
 export default function Login() {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleClickShowPassword = () => setShowPassword(!showPassword);
+    const handleMouseDownPassword = () => setShowPassword(!showPassword);
     const { appState, setAppState } = useAppStateContext()
     const navigate = useNavigate()
     const [isProcessing, setIsProcessing] = useState(false)
@@ -61,67 +113,172 @@ export default function Login() {
 
     const paperStyle = {
         padding: 20,
-        height: "70vh",
-        width: 280,
+        height: "75vh",
+        width: 300,
         margin: "20px auto"
     }
+    const classes = useStyles()
+
     return (
-        <div>
+        <div className={classes.mainBackground}>
             {invalid?<Box mt={3}><Typography align='center' color='error'>Invalid email/password, please try again</Typography></Box>:<div></div>}
             <Typography></Typography>
-            <Paper elevation={10} style={paperStyle}>
+            <Box align="center"  p={4} 
+                    display="flex"
+                    justifyContent="center"
+                    // position= "absolute"
+                    alignItems="center"
+                    top="10px"
+                    right="50px">
+                            <Typography align="left"style={{zIndex: "4 !important"}} className={classNames("title-font",classes.title, classes.overlay)} >
+                            Simply Pizza .
+                            </Typography>
+                            <Typography align="left"style={{zIndex: "5 !important"}} className={classNames("shadow", "title-font", classes.title, classes.overlay2)} >
+                            Simply Pizza .
+                            </Typography>
+                    </Box>
+                    <Divider className={classes.blue}/>
+            <Paper elevation={10} style={paperStyle} className={classes.outlinePaper} variant="outlined" m={2}>
                 <Grid align="center">
-                    <Avatar>
-                        <LockOutlinedIcon />
-                    </Avatar>
-                    <h2>
+                    <h1>
                         Sign In
-                    </h2>
+                    </h1>
+                    <Typography style={{fontSize:'15px',
+                            fontWeight: 'bold',}}> 
+                    New to Simply Pizza?  <Link to="/signup">Sign Up</Link> 
+                    </Typography>
+                     <Box m={1} >
+                        <Button 
+                        type = 'submit' 
+                        variant = "contained" 
+                        color = 'primary'
+                        style= {{borderRadius: 25, 
+                            backgroundColor: "#4285F7",
+                            color: "white",
+                            border: "2px solid #000",
+                            textTransform: "none",
+                            fontWeight: 'bold',
+                        }} 
+                        fullWidth
+                        > <FcGoogle/> &nbsp;&nbsp; Continue with Google
+                        </Button>
+                        </Box>
+                        <Box m={1} >
+                        <Button 
+                        type = 'submit' 
+                        variant = "contained" 
+                        color = 'primary'
+                        style= {{borderRadius: 25, 
+                            backgroundColor: "#010100",
+                            color: "white",
+                            border: "2px solid #000",
+                            textTransform: "none",
+                            fontWeight: 'bold',
+                        }} 
+                        fullWidth
+                        > <FaApple/> &nbsp;&nbsp; Continue with Apple
+                        </Button>
+                        </Box>
+                        <Box m={2} >
+                    <DividerWithText>
+                        <Typography align="left"style={{color: "#777677", fontSize:'14px'}}  >
+                        or continue with email
+                        </Typography>
+                    </DividerWithText>
+                    </Box>
                 </Grid>
+                <Grid container direction={"column"} spacing={1}>
+                <Grid item >  
                 <TextField
                     onChange={handleOnInputChange}
                     name="email"
-                    label="email"
-                    placeholder="Enter email"
+                    label="Email"
+                    placeholder=""
                     fullWidth
                     required
+                    InputLabelProps={{
+                        shrink: true,
+                        style: {
+                            color: "#000",
+                            fontWeight: 'bold'
+                        }
+                      }}
+                    variant="outlined"
+                    InputProps={{
+                        classes: {
+                          notchedOutline: classes.notchedOutline
+                        }
+                      }}
                 />
+                </Grid>
+                <Grid item >  
                 <TextField
                     onChange={handleOnInputChange}
                     name="password"
                     label="Password"
-                    placeholder="Enter password"
+                    placeholder=""
                     type="password"
                     fullWidth
                     required
+                    InputLabelProps={{
+                        shrink: true,
+                        style: {
+                            color: "#000",
+                            fontWeight: 'bold'
+                        }
+                      }}
+                    variant="outlined"
+                    type={showPassword ? "text" : "password"} // <-- This is where the magic happens
+                    InputProps={{
+                        classes: {
+                          notchedOutline: classes.notchedOutline
+                        },
+                        endAdornment: (
+                            <InputAdornment position="end">
+                            <IconButton
+                            aria-label="toggle password visibility"
+                            onClick={handleClickShowPassword}
+                            onMouseDown={handleMouseDownPassword}
+                          >
+                          {showPassword ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                      </InputAdornment>
+                       )
+                       }}
                 />
+                </Grid>
                 <FormControlLabel
                     control={
                         <Checkbox
                             // checked={state.checkedB}
                             // onChange={handleChange}
                             name="checkedB"
-                            color="primary"
+                            style={{color:"red"}}
+                            
                         />
                     }
                     label="Remember Me"
+                    style={{color:"black"}}
                 />
+                </Grid>
+                <Box mt={4}>
                 <Button
                     onClick={handleOnSubmit}
                     type='submit'
                     variant="contained"
                     color='primary'
+                    style= {{borderRadius: 25, 
+                        backgroundColor: "#2EDBFD",
+                        fontWeight: 'bold',
+                        color: "black",
+                        border: "2px solid #000"
+                    }} 
                     fullWidth
                 >Sign in
                 </Button>
-                <Typography>
-                    Do you have an account?
-                    <Link href="/signup">
-                        Sign Up
-                    </Link>
-                </Typography>
-
+                </Box>
             </Paper>
+            <Divider className={classes.blue}/>
         </div>
     )
 }
